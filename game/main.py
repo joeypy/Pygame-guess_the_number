b6e31cefc8f6350546b4983b0cf919258c69976c
Joseph Boscan <joeypc.py@gmail.com>
2021-09-15 20:42:10 -0400

    Revert "Finished the game"
    This reverts commit 2795437c07c5ed46e7b1fd9c658da224aae8cdee.

--- game/main.py
+++ game/main.py
@@ -4,7 +4,7 @@
 '''
 '''
 Requirement:
- 1. Show the number range form 1 to 100
+ 1. Change the number range form 1 to 100
  2. Game should ask us to guess a number
  3. Give a clue of the number is higher or lower than the guess
  4. Inform the player if he won
